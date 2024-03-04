392b4d424ef9cd63c3768d22c5339c44ccde7a2e
nadiaalya02 <nadiaalya07@gmail.com>
2024-03-03 20:54:09 -0800

    rev8

--- lat1.py
+++ lat1.py
@@ -3,6 +3,7 @@ from page1 import page_1
 from page2 import page_2
 from page3 import page_3
 from page4 import page_4
+from page5 import page_5
 #import pandas as pd
 # st.write("Anime Favorit")
 # df = pd.DataFrame({
@@ -17,7 +18,8 @@ PAGES = {
     "Page 1" : page_1,
     "Page 2" : page_2,
     "Page 3" : page_3,
-    "Page 4" : page_4
+    "Page 4" : page_4,
+    "Kalkulator Luas" : page_5
 }
 
 st.sidebar.image("nadiaalya.jpeg", width=200)
--- page4.py
+++ page4.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def page_4():
     st.title("Hayoloooo")
-    st.write("Halaman ini digunakan untuk Rumus Matematika. Jadi, sesudah menonton anime yukk belajar matematika bersama Nadia Alya^-^")
+    st.write("Halaman ini digunakan untuk Rumus Matematika Part 1. Jadi, sesudah menonton anime yukk belajar matematika bersama Nadia Alya^-^")
     
     with open('rumus.md','r') as file:
         isi_teks = file.read()
